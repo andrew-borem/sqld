a30919b4c19d45a715dc0116285405419c246b13
andrew-borem <aborem@dynamixwebdesign.com>
2024-07-22 12:46:03 -0500

    updating flake

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     { self, nixpkgs, ... }:
     {
 
-      libsql = nixpkgs.legacyPackages."x86_64-linux".rustPlatform.buildRustPackage rec {
+      default = nixpkgs.legacyPackages."x86_64-linux".rustPlatform.buildRustPackage rec {
         pkgs = nixpkgs.legacyPackages."x86_64-linux";
         pname = "sqld";
         version = "0.24.17";
