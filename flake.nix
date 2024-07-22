946238834d08b70ed07ec8a0bfaeca01b25db57e
andrew-borem <aborem@dynamixwebdesign.com>
2024-07-22 14:45:23 -0500

    adding in cli and php extension

--- flake.nix
+++ flake.nix
@@ -50,8 +50,48 @@
         # requires a complex setup with podman for the end-to-end tests
         doCheck = false;
       };
+      /*
+            php-extension = self.pkgs.rustPlatform.buildRustPackage rec {
+              pname = "sqld";
+              version = "0.24.17";
 
-      turso-cli = self.pkgs.buildGoModule rec {
+              src = self.pkgs.fetchFromGitHub {
+                owner = "tursodatabase";
+                repo = "libsql";
+                rev = "libsql-server-v${version}";
+                hash = "sha256-EPvrKUXCUwbM+VU7YbeoZK4PLjh5NgYnB18sCG5Eb3I=";
+              };
+
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+                outputHashes = {
+                  "console-api-0.5.0" = "sha256-MfaxtzOqyblk6aTMqJGRP+123aK0Kq7ODNp/3lehkpQ=";
+                  "hyper-rustls-0.24.1" = "sha256-dYN42bnbY+4+etmimrnoyzmrKvCZ05fYr1qLQFvzRTk=";
+                  "rheaper-0.2.0" = "sha256-u5z6J1nmUbIQjDDqqdkT0axNBOvwbFBYghYW/r1rDHc=";
+                  "s3s-0.10.1-dev" = "sha256-y4DZnRsQzRNsCIp6vrppZkfXSP50LCHWYrKRoIHYPik=";
+                };
+              };
+
+              nativeBuildInputs = [
+                self.pkgs.pkg-config
+                self.pkgs.protobuf
+                self.pkgs.rustPlatform.bindgenHook
+                self.pkgs.sqlite
+                self.pkgs.cmake
+                self.pkgs.libclang
+              ];
+
+              buildInputs =
+                [ self.pkgs.openssl ]
+                ++ self.pkgs.lib.optionals self.pkgs.stdenv.isDarwin [
+                  self.pkgs.darwin.apple_sdk.frameworks.Security
+                ];
+
+              # requires a complex setup with podman for the end-to-end tests
+              doCheck = false;
+            };
+      */
+      cli = self.pkgs.buildGoModule rec {
         pname = "turso-cli";
         version = "0.96.2";
 
